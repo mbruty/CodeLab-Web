38a5ae0fcccd9522e05134934ad00f693197adb9
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2022-10-11 12:17:11 +0100

    Auto generated graphql types from API

--- src/gql/types/gql.ts
+++ src/gql/types/gql.ts
@@ -1,16 +1,18 @@
 /* eslint-disable */
 import * as types from './graphql';
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import type {TypedDocumentNode as DocumentNode} from '@graphql-typed-document-node/core';
 
 const documents = {
-    "\n\tquery helloQuery {\n\t\thello\n\t}\n": types.HelloQueryDocument,
+	"\n\tquery AuthCheck {\n\t\tme {\n\t\t\tid\n\t\t\temail\n\t\t\tusername\n\t\t\txp\n\t\t}\n\t}\n": types.AuthCheckDocument,
+	"\n\tmutation LoginMutation($email: String!, $password: String!) {\n\t\tlogin(email: $email, password: $password) {\n\t\t\tid\n\t\t\temail\n\t\t\tusername\n\t\t\trefreshCount\n\t\t}\n\t}\n": types.LoginMutationDocument,
 };
 
-export function graphql(source: "\n\tquery helloQuery {\n\t\thello\n\t}\n"): (typeof documents)["\n\tquery helloQuery {\n\t\thello\n\t}\n"];
+export function graphql(source: "\n\tquery AuthCheck {\n\t\tme {\n\t\t\tid\n\t\t\temail\n\t\t\tusername\n\t\t\txp\n\t\t}\n\t}\n"): (typeof documents)["\n\tquery AuthCheck {\n\t\tme {\n\t\t\tid\n\t\t\temail\n\t\t\tusername\n\t\t\txp\n\t\t}\n\t}\n"];
+export function graphql(source: "\n\tmutation LoginMutation($email: String!, $password: String!) {\n\t\tlogin(email: $email, password: $password) {\n\t\t\tid\n\t\t\temail\n\t\t\tusername\n\t\t\trefreshCount\n\t\t}\n\t}\n"): (typeof documents)["\n\tmutation LoginMutation($email: String!, $password: String!) {\n\t\tlogin(email: $email, password: $password) {\n\t\t\tid\n\t\t\temail\n\t\t\tusername\n\t\t\trefreshCount\n\t\t}\n\t}\n"];
 
 export function graphql(source: string): unknown;
 export function graphql(source: string) {
-  return (documents as any)[source] ?? {};
+	return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
