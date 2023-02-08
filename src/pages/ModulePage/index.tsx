6e89e03c7f163535f8fa2614ce7bb79480e43de3
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2023-02-08 09:36:52 +0000

    Created module page

--- src/pages/ModulePage/index.tsx
+++ src/pages/ModulePage/index.tsx
@@ -1,12 +1,21 @@
 import "./index.tsx.css";
 import React, { FC, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import UserContext from "../../contexts/UserContext";
 import { graphql } from "gql";
 import { unauthorisedCheck } from "gql/exceptionChecks";
-import { Card } from "@chakra-ui/react";
-
+import {
+  Card,
+  Center,
+  Grid,
+  Heading,
+  HStack,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const getModuleQuery = graphql(`
   query getModule($moduleId: Int!) {
@@ -16,26 +25,77 @@ const getModuleQuery = graphql(`
       tasks {
         id
         title
-        description
         availableLanguages
       }
     }
   }
 `);
 
+function langToColour(lang: string | null): Array<string> {
+  switch (lang) {
+    case "Python":
+      return ["blue.600", "white"];
+    default:
+      return ["red.100", "black.100"];
+  }
+}
+
 const ModulePage: FC = (props) => {
   const { moduleid } = useParams();
-  const { data, error, loading } = useQuery(getModuleQuery);
+  const { data, error, loading } = useQuery(getModuleQuery, {
+    variables: {
+      moduleId: parseInt(moduleid as string),
+    },
+  });
   const userObserver = useContext(UserContext);
 
   useEffect(() => {
     unauthorisedCheck(error, userObserver);
   }, [error, userObserver]);
 
+  if (loading) {
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
+  }
+
   return (
-    <Card>
-    </Card>
-  )
+    <>
+      <Heading as="h1">{data?.module.title}</Heading>
+      <Text>{data?.module.description}</Text>
+      <Grid gridTemplateColumns="repeat(4, 1fr)" marginTop="2" gap="2">
+        {data?.module.tasks?.map((task) => (
+          <Link to={`/code/${task?.id}`}>
+            <Card padding="0.5" paddingLeft="2">
+              <Heading fontSize="xx-large" as="h2">
+                {task?.title}
+              </Heading>
+              <HStack marginTop="1">
+                {task?.availableLanguages?.map((lang) => (
+                  <Card
+                    padding="0.5"
+                    paddingLeft="2"
+                    paddingRight="2"
+                    backgroundColor={langToColour(lang)[0]}
+                    color={langToColour(lang)[1]}
+                    borderRadius="xl"
+                  >
+                    <Text>{lang}</Text>
+                  </Card>
+                ))}
+              </HStack>
+              <FontAwesomeIcon
+                style={{ position: "absolute", right: "10px", top: "50%", transform: "translateY(-50%) " }}
+                icon={faChevronRight}
+              />
+            </Card>
+          </Link>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default ModulePage;
